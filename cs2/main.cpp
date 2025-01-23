cc2081a0ebe99565a509e0fca606e3affb09e978
dmitriyscor <88781754+dmitriyscor@users.noreply.github.com>
2025-01-23 14:25:04 -0800

    Initial Commit

---
+++ cs2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cs2
+//
+//  Created by Dmytro Gozha on 1/23/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
